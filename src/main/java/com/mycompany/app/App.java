edaa6895d3d355aef499df72d77e62d1dc3f8507
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-12-05 17:27:55 +0000

    extracted method

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( getMessage() );
+    }
+
+    public static String getMessage() {
+        return "Hello World!";
     }
 }
